c6d1a640b40f49e97a59c483a64ac13146fc7e8e
Administrador <jadersonrs@outlook.com>
2024-03-31 09:58:54 -0300

    Create Task Model

--- todosimple/src/main/java/com/rochajaderson/todosimple/models/User.java
+++ todosimple/src/main/java/com/rochajaderson/todosimple/models/User.java
@@ -5,10 +5,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -44,7 +48,8 @@ public class User {
     @Size(groups = CreateUser.class, min = 8, max = 60)
     private String password;
 
-    // private List<Task> tasks = new ArrayList<Task>();
+    @OneToMany(mappedBy = "user")
+    private List<Task> tasks = new ArrayList<Task>();
 
     public User() {
     }
