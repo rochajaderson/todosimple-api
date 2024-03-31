1ef5764065574a274664a59948091a62b6e54d79
Administrador <jadersonrs@outlook.com>
2024-03-31 10:33:07 -0300

    Create User and Tasks Repositories

---
+++ todosimple/src/main/java/com/rochajaderson/todosimple/repositories/TaskRepository.java
@@ -0,0 +1,15 @@
+package com.rochajaderson.todosimple.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rochajaderson.todosimple.models.Task;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findByUser_Id(Long id);
+    
+}
